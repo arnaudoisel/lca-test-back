67b9b50ef89670c9e9a733c5405710f922ec2a29
Arnaud Oisel <arnaud.oisel@gmail.com>
2020-09-22 19:51:43 +0200

    Adds currency converter that uses graph and input

---
+++ LuccaDevises/CurrencyConverter.cs
@@ -0,0 +1,49 @@
+﻿using LuccaDevises.graph;
+using System;
+using System.Collections.Generic;
+
+namespace LuccaDevises
+{
+    public class CurrencyConverter
+    {
+		private readonly decimal amount;
+		private readonly Graph graph;
+		private readonly Vertex source;
+		private readonly Vertex destination;
+		private readonly CurrencyChangeRates currencyChangeRates;
+
+		public CurrencyConverter(Input input)
+        {
+			this.amount = input.Amount;
+			this.currencyChangeRates = input.CurrencyChangeRates;
+			this.graph = input.CurrencyChangesGraph;
+			this.source = new Vertex(input.SourceCurrency);
+			this.destination = new Vertex(input.DestinationCurrency);
+        }
+
+		/// <summary>
+		/// Convert an amount of money in a given currency to the desired currency.
+		/// </summary>
+		/// <remarks>
+		/// It uses a Breadth-First algorithm to search for the shortest path between two currencies.
+		/// </remarks>
+		/// <returns>The amount in the desired destination currency.</returns>
+        public decimal Convert()
+        {
+			List<Vertex> path = new BreadthFirstSearch().FindShortestPath(graph, source, destination);
+			return ConvertAmount(amount, path);
+		}
+
+		private decimal ConvertAmount(decimal amount, List<Vertex> path)
+		{
+			for (int i = 0; i < path.Count - 1; i++)
+			{
+				Vertex source = path[i];
+				Vertex destination = path[i + 1];
+				double rate = currencyChangeRates.Get(source.label, destination.label);
+				amount = decimal.Round(amount * (decimal)rate, 4, MidpointRounding.AwayFromZero);
+			}
+			return decimal.Round(amount, 0, MidpointRounding.AwayFromZero);
+		}
+	}
+}
---
+++ LuccaDevisesTests/CurrencyConverterTests.cs
@@ -0,0 +1,51 @@
+﻿using LuccaDevises;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuccaDevisesTests
+{
+    [TestClass]
+    public class CurrencyConverterTests
+    {
+        [TestMethod]
+        public void Convert_ShouldConvertAmount()
+        {
+            var input = InputBuilder.GetInstance()
+                .ReadFirstLine("EUR;550;JPY")
+                .ReadCurrencyChangeRatesLines(new string[] {
+                    "6",
+                    "AUD;CHF;0.9661",
+                    "JPY;KRW;13.1151",
+                    "EUR;CHF;1.2053",
+                    "AUD;JPY;86.0305",
+                    "EUR;USD;1.2989",
+                    "JPY;INR;0.6571"
+                })
+                .Build();
+            var currencyConverter = new CurrencyConverter(input);
+
+            decimal chf = RoundAmount(550M * (decimal)1.2053);
+            double chfToAud = RoundRate(1 / 0.9661);
+            decimal aud = RoundAmount(chf * (decimal)chfToAud);
+            decimal jpy = RoundAmount(aud * (decimal)86.0305);
+            
+            decimal expectedResult = decimal.Round(jpy, 0, MidpointRounding.AwayFromZero);
+            decimal actualResult = currencyConverter.Convert();
+
+            Assert.AreEqual(expectedResult, actualResult,
+                "Amount converted is incorrect.");
+        }
+
+        private decimal RoundAmount(decimal money)
+        {
+            return decimal.Round(money, 4, MidpointRounding.AwayFromZero);
+        }
+
+        private double RoundRate(double rate)
+        {
+            return Math.Round(rate, 4);
+        }
+    }
+}
