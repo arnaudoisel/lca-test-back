8b85da01ee9dee5ade7b021c5efd900acb55c92f
Arnaud Oisel <arnaud.oisel@gmail.com>
2020-09-22 19:53:01 +0200

    Adds Main

---
+++ LuccaDevises/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace LuccaDevises
+{
+    /// <summary>
+    /// Entrypoint, class containing the Main method.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args == null || args.Length != 1)
+                    throw new ArgumentException("This program takes as input one argument only that is a file path.");
+
+                Input input = new FileReader().Read(args[0]);
+                var currencyConverter = new CurrencyConverter(input);
+                decimal result = currencyConverter.Convert();
+                Console.WriteLine(result);
+            }
+            catch (ArgumentException e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
